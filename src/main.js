cea7bf8d01733678ea1ef5c7ef4265f07aa60769
ostaily <ostaballero@gmail.com>
2024-10-17 00:53:26 -0500

    clase 041024

--- src/main.js
+++ src/main.js
@@ -1,27 +1,37 @@
 var app = new Vue({
-  el: '#app',
+  el:"#app",
   data: {
     respuesta: 0,
     numero1: null,
     numero2: null,
-    nameButton: 'Sumar'
+    nameButton : 'suma',
+    arrayFrutas :['manzana','pera','melocoton'],
+    info:[{"nombre":"Bastian","perfil":"Programador de Software","img":"./img1.jpg","hobby":['Juegos','Futbol']},
+         {"nombre":"Jesus","perfil":"Estudiante de Software","img":"./img2.jpg","hobby":['Caminar','Nadar']}
+  ],
+  cocteles: []
   },
-  methods: {
-    sumar() {
 
-      this.respuesta = this.numero1 + this.numero2
-    }
-  },
-  computed: {
-    colorRespuesta() {
-      return this.respuesta>400 ?'text-success':this.respuesta>100?'text-danger':'text-primary'
-      // if(this.respuesta>100 && this.respuesta<400){
-      //  return 'text-success'
-      //}else if(this.respuesta>400){
-      //return 'text-danger'
-      // }else{
-      // return 'text-primary'
-      // }
+  methods : {
+    sumar(){
+      this.respuesta= this.numero1+this.numero2
+    },
+    restar(){
+      this.respuesta= this.numero1-this.numero2
+
+    },
+    getDataCoktail(){
+       const url='https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
+       fetch(url)
+       .then(Response=>{
+        return Response.json()
+       })
+       .then(data=>{
+        this.cocteles=data.drinks || []
+       })
+       .catch(error=>{
+        console.log('Error',error);
+       })
     }
   }
 })
